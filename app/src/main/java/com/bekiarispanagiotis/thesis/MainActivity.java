37342ea54e9babb13f3f64163ff3fe5053a91337
BekPan <panosb00@gmail.com>
2023-01-05 12:59:13 +0200

    Base Fragment structure

--- app/src/main/java/com/bekiarispanagiotis/thesis/MainActivity.java
+++ app/src/main/java/com/bekiarispanagiotis/thesis/MainActivity.java
@@ -1,13 +1,23 @@
 package com.bekiarispanagiotis.thesis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private View maxView;
+    BottomNavigationView bottomNavigationView;
+    MainHomeFragment mainHomeFragment = new MainHomeFragment();
+    PianoFragment pianoFragment = new PianoFragment();
+    GuitarFragment guitarFragment = new GuitarFragment();
+    ViolinFragment violinFragment = new ViolinFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,30 @@ public class MainActivity extends AppCompatActivity {
                     maxView.setSystemUiVisibility(fullScreenView());
             }
         });
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,mainHomeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,mainHomeFragment).commit();
+                        return true;
+                    case R.id.piano:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,pianoFragment).commit();
+                        return true;
+                    case R.id.guitar:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,guitarFragment).commit();
+                        return true;
+                    case R.id.violin:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView,violinFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     @Override
