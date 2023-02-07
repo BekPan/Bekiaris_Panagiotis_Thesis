c29214a380b091d13651212da5d5e60143166af0
BekPan <panosb00@gmail.com>
2023-02-07 10:59:58 +0200

    Added launchActivity test and assertions fixes

--- app/src/androidTest/java/com/bekiarispanagiotis/thesis/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bekiarispanagiotis/thesis/ExampleInstrumentedTest.java
@@ -1,6 +1,5 @@
 package com.bekiarispanagiotis.thesis;
 
-import android.app.Activity;
 import android.content.Context;
 
 import androidx.lifecycle.Lifecycle;
@@ -30,16 +29,16 @@ public class ExampleInstrumentedTest {
     @Test
     public void launchActivity() {
         var activityScenario1 = ActivityScenario.launchActivityForResult(MainActivity.class);
-        assertEquals(true, activityScenario1.getState().equals(Lifecycle.State.RESUMED)); //  The activity became active and ready to receive input
+        assertEquals(activityScenario1.getState(), Lifecycle.State.RESUMED); //  The activity became active and ready to receive input
         activityScenario1.close();
         var activityScenario2 = ActivityScenario.launchActivityForResult(PianoActivity.class);
-        assertEquals(true, activityScenario2.getState().equals(Lifecycle.State.RESUMED));
+        assertEquals(activityScenario2.getState(), Lifecycle.State.RESUMED);
         activityScenario2.close();
         var activityScenario3 = ActivityScenario.launchActivityForResult(GuitarActivity.class);
-        assertEquals(true, activityScenario3.getState().equals(Lifecycle.State.RESUMED));
+        assertEquals(activityScenario3.getState(), Lifecycle.State.RESUMED);
         activityScenario3.close();
         var activityScenario4 = ActivityScenario.launchActivityForResult(ViolinActivity.class);
-        assertEquals(true, activityScenario4.getState().equals(Lifecycle.State.RESUMED));
+        assertEquals(activityScenario4.getState(), Lifecycle.State.RESUMED);
         activityScenario4.close();
     }
 }
